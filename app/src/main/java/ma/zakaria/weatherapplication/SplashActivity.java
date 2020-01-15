c32a87f596bdaa89dea45e4877394171ac04f28d
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2020-01-15 20:14:40 +0100

    First commit

--- app/src/main/java/ma/zakaria/weatherapplication/SplashActivity.java
+++ app/src/main/java/ma/zakaria/weatherapplication/SplashActivity.java
@@ -1,16 +1,15 @@
 package ma.zakaria.weatherapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
--- app/src/main/java/ma/zakaria/weatherapplication/interfaces/api.java
+++
@@ -1,5 +0,0 @@
-package ma.zakaria.weatherapplication.interfaces;
-
-public interface api {
-    String key_api = "1aae8b69408f2fd02210dcc6c29c3a67";
-}
